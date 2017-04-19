45d96c86ec68cec9395933083166d3ae60413928
Anwyl <andrew.martin@ruralsourcing.com>
2017-04-19 10:24:12 -0400

    Add files via upload

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public BoxCollider territory;
+    GameObject player;
+    public Transform target;
+    public float speed = 3f;
+
+    bool playerinTerritory;
+
+    public GameObject enemy;
+    Monster monster;
+
+
+    void Start()
+
+    {
+        Debug.Log(playerinTerritory);
+        player = GameObject.FindGameObjectWithTag("Player");
+        monster = enemy.GetComponent<Monster>();
+        playerinTerritory = false;
+    }
+
+    void Update()
+    {
+        
+        if(playerinTerritory == true)
+        {
+            transform.LookAt(target);
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerinTerritory = true;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour {
+
+    public Transform target;
+    public float speed = 3f;
+    public float attackRange = 1f;
+    public int attackDamage = 2;
+    public float timeBetweenAttacks;
+
+
+    public void MoveToPlayer()
+    {
+        transform.LookAt(target.position);
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+}
